7e3768eb6ad9f595561e037dc621610c8c2a5b73
Idel <idlch.perte@gmail.com>
2022-11-01 16:58:18 -0600

    Construcción inicial de app

---
+++ app/src/main/java/mx/unam/unaminternacional/dgecimovil/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.unam.unaminternacional.dgecimovil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
