ed7b39e058c9a3723308d00102b3ed68fb374c9e
Idelfonso De la Cruz <idlch.perte@gmail.com>
2022-11-10 19:29:20 -0600

    Animación log

--- app/src/main/java/mx/unam/unaminternacional/dgecimovil/MainActivity.kt
+++ app/src/main/java/mx/unam/unaminternacional/dgecimovil/MainActivity.kt
@@ -1,11 +1,20 @@
 package mx.unam.unaminternacional.dgecimovil
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //ANIMACION
+        var animacion = findViewById<ImageView>(R.id.loadIng)
+        animacion.setImageResource(R.drawable.loadb)
+        val anima = animacion.drawable as AnimatedVectorDrawable
+        anima.start()
+
     }
 }
