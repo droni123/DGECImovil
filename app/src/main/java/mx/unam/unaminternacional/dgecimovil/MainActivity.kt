876c515228f56006bec2220756fe5cb97770cc0a
Idel <idlch.perte@gmail.com>
2022-11-11 13:51:44 -0600

    Particulas JS & Típografia

--- app/src/main/java/mx/unam/unaminternacional/dgecimovil/MainActivity.kt
+++ app/src/main/java/mx/unam/unaminternacional/dgecimovil/MainActivity.kt
@@ -4,17 +4,38 @@ import android.graphics.drawable.AnimatedVectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import me.ibrahimsn.particle.ParticleView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var particleView: ParticleView
+    private lateinit var anima: AnimatedVectorDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView = findViewById<TextView>(R.id.loadTxt)
+        textView.text = HtmlCompat.fromHtml(getString(R.string.app_load),HtmlCompat.FROM_HTML_MODE_LEGACY)
+
         //ANIMACION
-        var animacion = findViewById<ImageView>(R.id.loadIng)
+        val animacion = findViewById<ImageView>(R.id.loadImg)
         animacion.setImageResource(R.drawable.loadb)
-        val anima = animacion.drawable as AnimatedVectorDrawable
+        anima = animacion.drawable as AnimatedVectorDrawable
         anima.start()
 
+        particleView = findViewById<ParticleView>(R.id.particleView)
+    }
+    override fun onResume() {
+        super.onResume()
+        anima.start()
+        particleView.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        anima.stop()
+        particleView.pause()
     }
 }
