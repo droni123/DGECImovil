76bc72726c1990c28836049023703e990e05c660
Idelfonso De la Cruz <idlch.perte@gmail.com>
2023-03-10 19:04:04 -0600

    Login parte 1

--- app/src/main/java/mx/unam/unaminternacional/dgecimovil/activitys/auth/AuthLoginFragment.kt
+++ app/src/main/java/mx/unam/unaminternacional/dgecimovil/activitys/auth/AuthLoginFragment.kt
@@ -3,6 +3,7 @@ package mx.unam.unaminternacional.dgecimovil.activitys.auth
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +12,9 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material3.AlertDialog
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
@@ -27,12 +26,12 @@ import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import me.ibrahimsn.particle.ParticleView
 import mx.unam.unaminternacional.dgecimovil.R
-import mx.unam.unaminternacional.dgecimovil.activitys.Home
 import mx.unam.unaminternacional.dgecimovil.databinding.FragmentInicioSecionBinding
 import mx.unam.unaminternacional.dgecimovil.ui.DGECITheme
 import mx.unam.unaminternacional.dgecimovil.ui.XaC
 import mx.unam.unaminternacional.dgecimovil.ui.Xbb
 import mx.unam.unaminternacional.dgecimovil.ui.Xbb25
+import java.util.regex.Pattern
 
 class AuthLoginFragment : Fragment() {
 
@@ -81,7 +80,12 @@ class AuthLoginFragment : Fragment() {
                     focusedLabelColor = Xbb,
                     textColor = Xbb
                 )
-            var usuario by rememberSaveable { mutableStateOf("") }
+                var usuario by rememberSaveable { mutableStateOf("") }
+                var pass by rememberSaveable { mutableStateOf("") }
+                var passVisible by rememberSaveable { mutableStateOf(false) }
+                var token by rememberSaveable { mutableStateOf("") }
+                var openDialog = remember { mutableStateOf(false)  }
+                var mensDialig = remember { mutableStateOf("") }
                 OutlinedTextField(
                     value = usuario,
                     onValueChange = { usuario = it },
@@ -93,8 +97,6 @@ class AuthLoginFragment : Fragment() {
                     },
                     modifier = Modifier.padding(15.dp)
                 )
-                var pass by rememberSaveable { mutableStateOf("") }
-                var passVisible by rememberSaveable { mutableStateOf(false) }
                 OutlinedTextField(
                     value = pass,
                     onValueChange = { pass = it },
@@ -104,9 +106,7 @@ class AuthLoginFragment : Fragment() {
                     visualTransformation = if (passVisible) VisualTransformation.None else PasswordVisualTransformation(),
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                     trailingIcon = {
-                        val image = if (passVisible)
-                            Icons.Filled.Visibility
-                        else Icons.Filled.VisibilityOff
+                        val image = if (passVisible)  Icons.Filled.Visibility  else Icons.Filled.VisibilityOff
                         val description = if (passVisible) "${getString(R.string.esconder)} ${getString(R.string.authPass)}" else "${getString(R.string.mostrar)} ${getString(R.string.authPass)}"
                         IconButton(onClick = {passVisible = !passVisible}){
                             Icon(imageVector  = image, description)
@@ -118,7 +118,6 @@ class AuthLoginFragment : Fragment() {
                     },
                     modifier = Modifier.padding(15.dp)
                 )
-                var token by rememberSaveable { mutableStateOf("") }
                 OutlinedTextField(
                     value = token,
                     onValueChange = { token = it },
@@ -132,16 +131,44 @@ class AuthLoginFragment : Fragment() {
                 )
                 ElevatedButton(
                     onClick = {
-                        Log.e("DRONI",usuario)
-                        Log.e("DRONI",pass)
-                        Log.e("DRONI",token)
-
+                        var iniciar_login = true
+                        mensDialig.value = ""
+                        if( usuario.isEmpty() ){
+                            iniciar_login = false
+                            mensDialig.value += "${getString(R.string.el_campo)} ${getString(R.string.authCorreo)} / ${getString(R.string.authNcuenta)} ${getString(R.string.obligatorio)}"
+                        }
+                        if( pass.isEmpty() ){
+                            iniciar_login = false
+                            mensDialig.value += (if (mensDialig.value.isEmpty()) "" else "\n")+"${getString(R.string.el_campo)} ${getString(R.string.authPass)} ${getString(R.string.obligatorio)}"
+                        }
+                        if( token.isEmpty() ){
+                            iniciar_login = false
+                            mensDialig.value += (if (mensDialig.value.isEmpty()) "" else "\n")+"${getString(R.string.el_campo)} ${getString(R.string.authToken)} ${getString(R.string.obligatorio)}"
+                        }
+                        if(iniciar_login){
+                            val pattern_ncta = Pattern.compile( "^[0-9]{9}$", Pattern.UNICODE_CASE)
+                            val is_emal = Patterns.EMAIL_ADDRESS.matcher(usuario).matches()
+                            val is_n_cuenta = pattern_ncta.matcher(usuario).matches() && usuario.length == 9
+                            if( !is_emal && !is_n_cuenta ){
+                                iniciar_login = false
+                                mensDialig.value += "${getString(R.string.el_campo)} ${getString(R.string.authCorreo)} / ${getString(R.string.authNcuenta)} ${getString(R.string.debe_ser_valido)}"
+                            }
+                        }
+                        if(!iniciar_login){
+                            openDialog.value = true
+                        }else{
+                            Log.e("DRONI",usuario)
+                            Log.e("DRONI",pass)
+                            Log.e("DRONI",token)
+                            /*
                         //Handler(Looper.getMainLooper()).postDelayed({
                             val vistaWelcome = Intent(context, Home::class.java)
                             startActivity(vistaWelcome)
                             activity?.finish()
                             activity?.overridePendingTransition( 0, R.drawable.fade_screen );
                         //}, 500)
+                        */
+                        }
                     },
                     modifier = Modifier.padding(15.dp),
                     colors = ButtonDefaults.buttonColors(
@@ -155,6 +182,32 @@ class AuthLoginFragment : Fragment() {
                     )
                     Icon(imageVector = Icons.Filled.Login,getString(R.string.iniciar_sesion))
                 }
+                ////////////////ALERT
+                if (openDialog.value) {
+                    Log.e("DRONI",mensDialig.value)
+                    openDialog.value = false
+                    /*
+                    AlertDialog(
+                        onDismissRequest = { openDialog.value = false },
+                        title = {
+                            Text(text = getString(R.string.advertencia))
+                        },
+                        text = {
+                            Text( mensDialig.value )
+                        },
+                        dismissButton = {
+                            Button(
+                                onClick = {
+                                    openDialog.value = false
+                                }) {
+                                Text(getString(R.string.aceptar))
+                            }
+                        }
+                    )
+                    */
+                }
+
+
             }
         }
     }
